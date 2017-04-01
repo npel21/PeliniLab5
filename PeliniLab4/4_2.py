90b34a6db8d27dcd371811c00c5460122958a360
nickp <pelinin1@udayton.edu>
2017-04-01 10:38:11 -0400

    Updated scripts for problems 2 and 3.

--- PeliniLab4/4_2.py
+++ PeliniLab4/4_2.py
@@ -1,21 +1,13 @@
 # Problem 2
-ea = ScreenEA()
 
-# Loop through all the functions
-for f in Functions(SegStart(ea), SegEnd(ea)):
-    
-    # Get the name of the function being called
-    f_name = GetFunctionName(f)
-    
-    # For each of the incoming references
-    for ref in CodeRefsTo(f, 0):    # Get a list of code references to 'ea'
-       
-        # Name of the function
-        caller_name = GetFunctionName(ref)
-        # Address that makes the call
-        caller_addr = GetFunctionAttr(ref, FUNCATTR_START)  # Get a function attribute
-        print caller_name, hex(caller_addr), f_name
-        
-        # Function called so print info
-        if f_name == "strcpy" or f_name == "sprintf" or f_name == "strncpy" or f_name == "wcsncpy" or f_name == "swprintf":
-            print caller_name, hex(caller_addr), f_name
+func_list = ["strcpy", "sprintf", "strncpy", "wcsncpy", "swprintf"]
+
+for spec_func in func_list:
+    spec_addr = LocByName(spec_func)
+    if spec_addr == BADADDR:
+        print spec_func, "not found"
+    else:
+        # Supposed to retrieve function name from linear address, doesn't seem to work
+        spec_name = GetFunctionName(spec_addr)
+        spec_addr = hex(spec_addr)
+        print spec_name, ":", spec_addr, ":", spec_func
--- PeliniLab4/4_3.py
+++ PeliniLab4/4_3.py
@@ -1,39 +1,21 @@
-from sets import Set
-
 # Problem 3
+func_list = ["strcpy", "sprintf", "strncpy", "wcsncpy", "swprintf"]
 
-for i in range(GetEntryPointQty()): # Retrieve number of entry points
-    ord = GetEntryOrdinal(i)    # Retrieve entry point ordinal number
-    if ord == 0:    # 0 if entry point doesn't exist otherwise entry point ordinal
-        continue
-    addr = GetEntryPoint(ord)   # Retrieve entry point address
-    #print("Export[%s]:Address[%x]\n" % (GetFunctionName(addr), addr))
-
-# Loop through code refs
-for ref_from in CodeRefsFrom(addr, 0): # Get a list of code references from 'ea'
-    # Sets caller_name to function being called directly by exported function.
-    caller_name = GetFunctionName(ref_from)
-    # Print "Name of Exported Function:caller_name" if any of the desired functions are found.
-    if caller_name == "strcpy" or  caller_name == "sprintf" or caller_name == "strncpy" or caller_name == "wcsncpy" or caller_name == "swprintf":
-        print GetFunctionName(addr), caller_name
-    
-    
-    # First layer
-    for layer1 in CodeRefsFrom(ref_from, 0):
-        caller_name = GetFunctionName(layer1)
-        if caller_name == "strcpy" or  caller_name == "sprintf" or caller_name == "strncpy" or caller_name == "wcsncpy" or caller_name == "swprintf":
-            print GetFunctionName(addr), caller_name
-        
-        
-        # Second layer
-        for layer2 in CodeRefsFrom(layer1, 0):
-            caller_name = GetFunctionName(layer2)
-            if caller_name == "strcpy" or  caller_name == "sprintf" or caller_name == "strncpy" or caller_name == "wcsncpy" or caller_name == "swprintf":
-                print GetFunctionName(addr), caller_name
-            
-            
-            # Third layer
-            for layer3 in CodeRefsFrom(layer2, 0):
-                caller_name = GetFunctionName(layer3)
-                if caller_name == "strcpy" or  caller_name == "sprintf" or caller_name == "strncpy" or caller_name == "wcsncpy" or caller_name == "swprintf":
-                    print GetFunctionName(addr), caller_name
+# For each of the segments
+for seg in Segments():
+    # For each of the defined elements
+    for head in Heads(seg, SegEnd(seg)):
+        # If it's an instruction
+        if isCode(GetFlags(head)):
+            # Get the mnemonic and increment the count
+            mnem = GetMnem(head)
+            # Check if a function is called
+            if mnem == 'call':
+                # Retrieve function name
+                func_name = GetFunctionName(head)
+                # Get operand of an instruction
+                Opnd = GetOpnd(head, 0)
+                for func_call in func_list:
+                    if Opnd.find(func_call):
+                        func_addr = hex(head)
+                        print func_name, ':', func_addr, ':', func_call
